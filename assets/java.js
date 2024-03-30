f1919ef7e4c7a3018e4167c415c5b64852d7e164
danieltqza <danieltqza@gmail.com>
2024-03-29 21:00:27 -0500

    yea now is so close

--- assets/java.js
+++ assets/java.js
@@ -6,6 +6,11 @@ const sections2 = document.getElementById('section2')
 const sections3 = document.getElementById('section3')
 const sections4 = document.getElementById('section4')
 
+const proyects1 = document.getElementById('proyect1')
+const proyects2 = document.getElementById('proyect2')
+const proyects3 = document.getElementById('proyect3')
+const proyects4 = document.getElementById('proyect4')
+
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
@@ -31,8 +36,12 @@ lightmode.addEventListener('click', function(){
     document.body.style.backgroundColor = '#fff'
     document.body.style.color = 'black';
     sections1.style.backgroundColor = 'white'
-    sections1.style.backgroundColor = 'rgb(109 102 102 / 11%)'
-    sections2.style.backgroundColor = 'rgb(109 102 102 / 11%)'
-    sections3.style.backgroundColor = 'rgb(109 102 102 / 11%)'
-    sections4.style.backgroundColor = 'rgb(109 102 102 / 11%)'
+    sections1.style.backgroundColor = 'rgb(109 102 102 / 11.5%)'
+    sections2.style.backgroundColor = 'rgb(109 102 102 / 11.5%)'
+    sections3.style.backgroundColor = 'rgb(109 102 102 / 11.5%)'
+    sections4.style.backgroundColor = 'rgb(109 102 102 / 11.5%)'
+    proyects1.style.backgroundColor = 'rgb(109 102 102 / 11.5%)'
+    proyects2.style.backgroundColor = 'rgb(109 102 102 / 11.5%)'
+    proyects3.style.backgroundColor = 'rgb(109 102 102 / 11.5%)'
+    proyects4.style.backgroundColor = 'rgb(109 102 102 / 11.5%)'
 })
